8e9acaf13e1aaa83943f4ba9455ca5c0a9eb7dca
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-06-03 17:06:36 -0500

    create and seed tables

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO departments (department_name) 
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+("Sales Lead", 100000 ,1),  
+("Software Engineer", 120000, 2),
+("Lawyer", 190000, 4),
+("Accountant", 65000, 3),
+("Salesperson", 80000 , 1),
+("Lead Engineer", 150000, 2);
+
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+("Dean", "Daska", 6),
+("Tom", "Nick", 5),
+("Mamie", "Aoughston", 3),
+("Mary", "Ross", 4),
+("Melissa", "Daska", 2),
+("Mike", "Ross", 1);
