453bb8dad2a54c2fd640b72cfec9ce965d457323
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-06-04 08:38:40 -0500

    add functions to view departments, roles and employees

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var inquirer = require('inquirer');
-// add consoleTable
-// add mysql
+var consoleTable = require('console.table');
+var mysql = require('mysql');
 
 // add connection
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3001',
+    user: 'root',
+    password: 'root',
+    database: 'employees_db'
+});
 
 function runSearch() {
     inquirer
@@ -25,12 +32,33 @@ function runSearch() {
 // call runSearch()
 
 
-
 // add function to view all departments
+function viewDepartments() {
+    connection.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
+        if (err) throw err;
+    });   
+    runSearch();
+}
 
 // add function to view all roles
+function viewRoles() {
+    connection.query('SELECT * FROM role', function (err, results) {
+        console.table(results);
+        if (err) throw err;
+    });
+    runSearch();
+}
 
 // add function to view all employees
+function viewEmployees() {
+    var query = 'SELECT employee.id, employee.first_name, role.title, department.department_name AS department, role.salary FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id';
+    connection.query(query, function(err, results) {
+        console.table(results);
+        if (err) throw err;
+    });
+    runSearch();
+}
 
 // add function to add department
 
