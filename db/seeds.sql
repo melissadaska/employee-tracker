48aea09da05f66cb2615032db2424bcfc4f8d75e
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-06-05 12:08:04 -0500

    fix errors in add employee function

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO department (name) 
-VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal"), ("Owner");
 
 INSERT INTO roles (title, salary, department_id)
 VALUES 
@@ -8,15 +8,17 @@ VALUES
 ("Lawyer", 190000, 4),
 ("Accountant", 65000, 3),
 ("Salesperson", 80000 , 1),
-("Lead Engineer", 150000, 2);
+("Lead Engineer", 150000, 2),
+("CEO", 1000000, 5);
 
 
 
-INSERT INTO employee (first_name, last_name, roles_id)
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
 VALUES 
-("Dean", "Daska", 6),
-("Tom", "Nick", 5),
-("Mamie", "Aoughston", 3),
-("Mary", "Ross", 4),
-("Melissa", "Daska", 2),
-("Mike", "Ross", 1);
+("Dean", "Daska", 6, 7),
+("Tom", "Nick", 5, 6),
+("Mamie", "Aoughston", 3, 7),
+("Mary", "Ross", 4, 7),
+("Melissa", "Daska", 2, 1),
+("Mike", "Ross", 1, 7),
+("Boss", "Boss", 7, 7);
